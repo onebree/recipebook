65c5929d72b56eb50b6d95278220ecc47ecff23d
Hunter Stevens <onebree@gmail.com>
2016-04-17 16:52:45 -0400

    New and create actions for recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,18 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit!
+  end
+end
