d1377a5870a1005bbae15d21d54da8293ff57884
Hunter Stevens <onebree@gmail.com>
2016-04-25 10:02:23 -0400

    Finish CRUD for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,8 @@
 class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
   def new
     @recipe = Recipe.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  resources :recipes, only: [:new, :create, :edit, :update, :destroy]
+  resources :recipes, only: [:new, :create, :edit, :update, :destroy, :show]
 end
