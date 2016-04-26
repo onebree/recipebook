8d1d5516dfee3f22bf0bd6dc9ad39f996074c268
Hunter Stevens <onebree@gmail.com>
2016-04-26 14:30:50 -0400

    Forgot LOC

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,6 +32,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
+    redirect_to categories_path
   end
   
   private
