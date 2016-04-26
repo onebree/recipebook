3fcf84ea5218b61b6fc2b4278d5eb57bd030f93d
Hunter Stevens <onebree@gmail.com>
2016-04-26 14:29:52 -0400

    Categories edit, update, destroy actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,24 @@ class CategoriesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+  end
   
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   
   match "/", to: "home#index", via: [:get, :post]
 
-  resources :recipes, only: [:new, :create, :edit, :update, :destroy, :show]
-  resources :categories, only: [:index, :new, :create]
+  resources :recipes,    only: [:new, :create, :edit, :update, :destroy, :show]
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy]
 end
