d0b06ded88e02e68d4009e6f9a88f73ad7efd5a8
Hunter Stevens <onebree@gmail.com>
2016-04-26 10:15:29 -0400

    Show all recipes on blank search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if params[:search]
-      @recipes = Recipe.search(params[:search]).order(:title)
-    else
+    if params[:search].blank?
       @recipes = Recipe.order(:title)
+    else
+      @recipes = Recipe.search(params[:search]).order(:title)
     end
   end
 end
