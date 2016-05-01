7c44cf392f1bc1e23e6efb058edaed1c4c55860e
Hunter Stevens <onebree@gmail.com>
2016-05-01 16:17:42 -0400

    Validations for user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
