d59692354dbc38c03ac65d7c8029d5b572b586ed
Hunter Stevens <onebree@gmail.com>
2016-05-01 17:09:28 -0400

    Fine-tune email format regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
  
   before_save { self.email = email.downcase }
 
