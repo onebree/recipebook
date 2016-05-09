6c37f441c63bb367afbbdb2967bf68435800c327
Hunter Stevens <onebree@gmail.com>
2016-05-09 13:24:39 -0400

    Change name to Recipe Kitchen in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Recipe Book"
+    base_title = "Recipe Kitchen"
     if page_title.empty?
       base_title
     else
